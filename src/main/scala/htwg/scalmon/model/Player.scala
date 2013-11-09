8be7e1fc0a45cedac80ab593bd062a31ca90a58e
ThorimMB <marc.bumiller@freenet.de>
2013-11-09 16:31:08 +0100

    added beaten and animalsAlive methods to Player

--- src/main/scala/htwg/scalmon/model/Player.scala
+++ src/main/scala/htwg/scalmon/model/Player.scala
@@ -5,4 +5,8 @@ import htwg.scalmon.utils.ImageLoader
 class Player(val name: String, val animals: Array[Animal]) {
 
   lazy val image = ImageLoader.get(name)
+
+  def beaten = animals.forall(!_.alive)
+
+  def animalsAlive = animals.toList.filter(_.alive)
 }
--- src/test/scala/htwg/scalmon/model/PlayerSpec.scala
+++ src/test/scala/htwg/scalmon/model/PlayerSpec.scala
@@ -3,7 +3,7 @@ package htwg.scalmon.model
 import org.scalatest._
 import scala.language.reflectiveCalls
 
-class PlayerSpec extends FlatSpec with Matchers {
+class PlayerSpec extends FlatSpec with Matchers with GivenWhenThen {
 
   def fixture = new {
     val human = new Player("Human", Array(new Animal("A1")))
@@ -27,5 +27,33 @@ class PlayerSpec extends FlatSpec with Matchers {
     fixture.human.animals.length should be(1)
     fixture.ai.animals should not equal null
     fixture.ai.animals.length should be(3)
+  }
+
+  it should "be can beaten" in {
+    Given("some healthy animals")
+    val ps = fixture
+
+    Then("the player should not be beaten")
+    ps.human.beaten should be(false)
+    ps.human.animalsAlive should be(ps.human.animals)
+    ps.ai.beaten should be(false)
+    ps.ai.animalsAlive should be(ps.ai.animals)
+
+    When("some of the animals are knocked out")
+    ps.ai.animals(1).healthPoints = 0
+
+    Then("the player should not be beaten too")
+    ps.ai.beaten should be(false)
+    ps.ai.animalsAlive should not be (ps.ai.animals)
+    ps.ai.animalsAlive.length should be(2)
+
+    When("all of the animals are knocked out")
+    (ps.human.animals ++ ps.ai.animals).foreach(_.healthPoints = 0)
+
+    Then("the player should be beaten")
+    ps.human.beaten should be(true)
+    ps.human.animalsAlive.length should be(0)
+    ps.ai.beaten should be(true)
+    ps.ai.animalsAlive.length should be(0)
   }
 }
